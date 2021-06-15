4f4210d52cbc3bab7c7cb975ef26f6eeaba022cc
dragsec <draganyes@gmail.com>
2021-06-15 14:53:57 +0200

    pt 2

---
+++ index2.sql
@@ -0,0 +1,32 @@
+/* 1 ospiti nascita */
+SELECT COUNT(`id`) AS "ospiti_totali", YEAR(`date_of_birth`)  AS 'year'
+FROM `ospiti`
+GROUP BY YEAR(`date_of_birth`);
+
+/* 2 status */
+SELECT `status`, 
+SUM(`price`)
+FROM `pagamenti`
+GROUP BY `status`;
+
+/* 3 conta prenotazioni stanza */
+SELECT `stanza_id`, 
+COUNT(`id`)
+FROM `prenotazioni`
+GROUP BY `stanza_id`
+ORDER BY COUNT(`stanza_id`) DESC;
+
+/* 4 ore di punta */
+SELECT `created_at`, 
+HOUR(`created_at`), 
+COUNT(`id`) AS "num_reservations_per_hour"
+FROM `prenotazioni`
+GROUP BY HOUR(`created_at`)
+ORDER BY 'prenotazioni' DESC;
+
+/* 5 record prenotazioni singola persona */
+SELECT `ospite_id`, 
+COUNT(*)
+FROM `prenotazioni_has_ospiti`
+GROUP BY `ospite_id`
+ORDER BY COUNT(`id`) DESC;
---
+++ index3.sql
@@ -0,0 +1,16 @@
+/* 1 nome ospiti con > 2 prenotazioni */
+
+
+/* 2 ospiti per prenotazione */
+
+
+/* 3 may 2018 -> nome, cognome, prezzo, pagamento */
+
+
+/* 4 $ totali prenotazioni floor 1 */
+
+
+/* 5 tutti i dati fatture id 7 */
+
+
+/* 6 stanze mai prenotate */
